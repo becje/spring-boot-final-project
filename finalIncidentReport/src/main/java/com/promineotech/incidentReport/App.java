314dd0aa1e9ac13ba1649dcd9aca922006155ba6
Tram <tramtran.email@gmail.com>
2021-04-17 16:50:39 -0500

    final update

---
+++ finalIncidentReport/src/main/java/com/promineotech/incidentReport/App.java
@@ -0,0 +1,63 @@
+package com.promineotech.incidentReport;
+
+import java.security.Key;
+import java.util.Base64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@ComponentScan("com.promineotech.incidentReport")
+@SpringBootApplication
+public class App 
+{
+	public static void JWTStuff() {
+		
+    	Key key = Keys.secretKeyFor( SignatureAlgorithm.HS256 );
+    	
+    	String jwt = Jwts.builder()
+    					.claim( "role", "ADMIN" )
+    					.setSubject( "PROMINEO TECH JWT" )  
+    					.signWith( key )
+    					.compact();
+    	
+    	System.out.println( "Here is a JWT:" );
+    	System.out.println( jwt );
+    
+    	String encodedKey = Base64.getEncoder().encodeToString( key.getEncoded() );  
+    	System.out.println( "Here is a server key which has been Base64 encoded:" );
+    	System.out.println( encodedKey );
+    	
+    	byte[] decodedKey = Base64.getDecoder().decode( encodedKey ); 
+    
+    	System.out.println( Jwts.parser().setSigningKey( decodedKey ).parseClaimsJws( jwt ).getBody().getSubject().equals( "PROMINEO TECH JWT" ) );
+    
+    	Key toMakeJWTsWith = Keys.hmacShaKeyFor( decodedKey );
+	}
+	
+	public static void userAuthenticationStuff() {
+   
+    	String password = "incidentReport";
+    	System.out.println( "Hash of the password '" + password + "' using system-generated salt:" );
+    	String salt = BCrypt.gensalt();
+    	
+    	String hashAndSalt = BCrypt.hashpw( password, salt );
+    	System.out.println( hashAndSalt );
+    	
+    	
+    	System.out.println( BCrypt.checkpw( password, hashAndSalt ) );
+	}
+	
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+        JWTStuff();
+    	System.out.println();
+    	userAuthenticationStuff();
+    }
+}
---
+++ finalIncidentReport/src/main/java/com/promineotech/incidentReport/entity/Incident.java
@@ -0,0 +1,92 @@
+package com.promineotech.incidentReport.entity;
+
+
+import javax.persistence.Column;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Incident {
+
+	private Long id;
+	private String date;
+	private String time;
+	private String location;
+	private String incidentDescription;
+	private String injuryDescription;
+	
+	private IncidentCategory incidentCategory;
+
+//	@JsonIgnore
+	private Set<Employee> employees;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getIncidentDescription() {
+		return incidentDescription;
+	}
+	public void setIncidentDescription(String incidentDescription) {
+		this.incidentDescription = incidentDescription;
+	}
+	public String getInjuryDescription() {
+		return injuryDescription;
+	}
+	public void setInjuryDescription(String injuryDescription) {
+		this.injuryDescription = injuryDescription;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	@ManyToMany(mappedBy = "incidents")
+	public Set<Employee> getEmployees() {
+		return employees;
+	}
+	public void setEmployees(Set<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name= "incidentCategory_id", referencedColumnName= "id")
+	public IncidentCategory getIncidentCategory() {
+		return incidentCategory;
+	}
+	public void setIncidentCategory(IncidentCategory incidentCategory) {
+		this.incidentCategory = incidentCategory;
+	}
+	
+
+}
